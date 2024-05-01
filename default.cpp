bde4342db5a6b9842c92c445ef40e5928f575cbb
Youtong0826 <jimmy2007826@gmail.com>
2024-05-01 20:33:46 +0800

    default

---
+++ default.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define pii pair<int,int>
+#define pb(x) emplace_back(x)
+#define sz(x) (int) x.size()
+#define all(x) x.begin(), x.end()
+#define Youtong ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+
+signed main(){
+    Youtong;
+}
