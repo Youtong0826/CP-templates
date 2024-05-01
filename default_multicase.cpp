cb3d7d0e3db4add745e3f7762e966ee9a3fc9e48
Youtong0826 <jimmy2007826@gmail.com>
2024-05-01 20:33:39 +0800

    default-multicase

---
+++ default_multicase.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define pii pair<int,int>
+#define pb(x) emplace_back(x)
+#define sz(x) (int) x.size()
+#define all(x) x.begin(), x.end()
+#define Youtong ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+
+void solve(){
+
+}
+
+signed main(){
+    Youtong;
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
